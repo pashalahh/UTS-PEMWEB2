a6dbb4111228951af0061124b12fcd476eee58c8
pashalahh <ramdinpasha@gmail.com>
2026-05-22 23:54:56 +0700

    chore: ubah base url axios ke backend vercel

--- Invofet-Front/src/api/axiosInstance.ts
+++ Invofet-Front/src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000', 
+  baseURL: 'https://uts-pemweb-2-w4wu.vercel.app/', 
   withCredentials: true, 
   headers: {
     'Content-Type': 'application/json',
